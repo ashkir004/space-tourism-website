1518bac2621b306de5b327402cbab9477ae31f99
ashkir004 <ashkir004@gmail.com>
2026-06-30 22:57:24 +0300

    refactor navbar to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,26 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon-32x32.png';
 
+	import Navbar from "$lib/components/layout/Navbar.svelte";
+    import bgHomeDesktop from "$lib/assets/home/background-home-desktop.jpg";
+    import bgHomeTablet from "$lib/assets/home/background-home-tablet.jpg";
+    import bgHomeMobile from "$lib/assets/home/background-home-mobile.jpg";
+
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div class="overflow-hidden">
+	<picture class="fixed min-h-screen min-w-screen z-[-1]">
+		<source srcset={bgHomeDesktop} media="(min-width: 64rem)" />
+    <source srcset={bgHomeTablet} media="(min-width: 48rem)" />
+    <img src={bgHomeMobile} alt="Background" class="absolute inset-0 w-full h-full object-cover" />
+	</picture>
+<header>
+    <Navbar />
+</header>
+
 {@render children()}
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
-    import Navbar from "$lib/components/layout/Navbar.svelte";
-    import bgHomeDesktop from "$lib/assets/home/background-home-desktop.jpg";
-    import bgHomeTablet from "$lib/assets/home/background-home-tablet.jpg";
-    import bgHomeMobile from "$lib/assets/home/background-home-mobile.jpg";
+
 </script>
 
-<picture class="fixed min-h-screen min-w-screen z-[-1]">
-    <source srcset={bgHomeDesktop} media="(min-width: 64rem)" />
-    <source srcset={bgHomeTablet} media="(min-width: 48rem)" />
-    <img src={bgHomeMobile} alt="Background" class="absolute inset-0 w-full h-full object-cover" />
-</picture>
-<header>
-    <Navbar />
-</header>
-<main class="flex flex-col  h-screen w-screen">
-    <section class=" p-6 grid grid-cols-1 grid-rows-2 h-screen w-screen gap-12 
-        lg:grid-cols-2 lg:grid-rows-1 lg:gap-0 lg:justify-items-center lg:items-center">
 
-        <article class="text-center lg:text-start  flex flex-col gap-6 items-center justify-start md:justify-end 
+<main class="flex flex-col min-h-dvh w-screen">
+    <section class=" p-6 grid grid-cols-1 grid-rows-[auto_1fr] h-full w-full gap-12 
+        lg:grid-cols-2 lg:gap-0 lg:justify-items-center lg:items-center my-auto">
+
+        <article class="text-center lg:text-start flex flex-col gap-6 items-center justify-start md:justify-end 
             lg:justify-center lg:items-start">
             <p class="text-label text-primary-foreground/80 ">So, you want to travel to</p>
             <h1 class="text-display uppercase text-primary-foreground">Space</h1>
@@ -25,8 +15,9 @@
         </article>
 
         <div class=" flex flex-col items-center justify-start md:justify-start lg:justify-center">
-            <button class="bg-secondary text-secondary-foreground rounded-full min-h-36 min-w-36
-                md:min-h-68 md:min-w-68 aspect-ratio-circle
+            <button class="bg-secondary text-secondary-foreground rounded-full                 
+                min-h-36 min-w-36
+                md:min-h-68 md:min-w-68 
                 text-heading uppercase">
                 Explore
             </button>
